96e5f13ab17a5d2444c25803c40528e539ecc649
Jimmy Reichley <jimmyqpublik@gmail.com>
2014-04-24 01:46:52 -0400

    Render basic representation of board

--- src/multi_snake/core.clj
+++ src/multi_snake/core.clj
@@ -1,9 +1,11 @@
 (ns multi-snake.core
+  (:require multi-snake.board
+            multi-snake.ui)
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (let [board (multi-snake.board/board)]
+    (multi-snake.ui/render board)))
---
+++ src/multi_snake/ui.clj
@@ -0,0 +1,27 @@
+(ns multi-snake.ui
+  (:import
+    (java.awt Color Dimension)
+    (javax.swing JFrame JPanel)))
+
+(def CELL_SIZE 25)
+
+(defn- board->jpanel
+  [board]
+  (doto (JPanel.)
+    (.setBackground Color/RED)
+    (.setPreferredSize (Dimension. (* CELL_SIZE (:width board))
+                                   (* CELL_SIZE (:height board))))))
+
+(defn- board->jframe
+  [board]
+  (doto (JFrame. "Multi-Snake!")
+    (.add (board->jpanel board))
+    (.pack)
+    (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)))
+
+(defn render
+  [board]
+  (let [frame (board->jframe board)]
+    (.setVisible frame true))
+  nil)
+
