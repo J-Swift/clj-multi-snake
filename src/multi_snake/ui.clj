f6602d6034994b14354107c6abd95a49b515c512
Jimmy Reichley <jimmyqpublik@gmail.com>
2014-04-24 23:57:56 -0400

    Add basic grid layout to ui

--- src/multi_snake/ui.clj
+++ src/multi_snake/ui.clj
@@ -1,16 +1,31 @@
 (ns multi-snake.ui
   (:import
-    (java.awt Color Dimension)
-    (javax.swing JFrame JPanel)))
+    (java.awt Color Dimension GridLayout)
+    (javax.swing JFrame JPanel JLabel)))
 
 (def CELL_SIZE 25)
 
+(defn- new-cell
+  [n]
+  (JLabel. (str n)))
+
+(defn- fill-panel
+  [panel num-cells-wide num-cells-high]
+  (loop [n (* num-cells-wide num-cells-high)]
+    (if (= 0 n)
+      panel
+      (do
+        (.add panel (new-cell n))
+        (recur (- n 1))))))
+
 (defn- board->jpanel
-  [board]
-  (doto (JPanel.)
-    (.setBackground Color/RED)
-    (.setPreferredSize (Dimension. (* CELL_SIZE (:width board))
-                                   (* CELL_SIZE (:height board))))))
+  [{:keys [width height]}]
+  (let [panel (JPanel. (GridLayout. width height))]
+    (doto panel
+      (.setBackground Color/RED)
+      (.setPreferredSize (Dimension. (* CELL_SIZE width)
+                                     (* CELL_SIZE height))))
+    (fill-panel panel width height)))
 
 (defn- board->jframe
   [board]
