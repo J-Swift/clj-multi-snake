7f936b85191f66b0563730820d1daa81b8cd12f0
Jimmy Reichley <jimmyqpublik@gmail.com>
2014-05-07 00:11:10 -0400

    Convert UI to use seesaw

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "This is a unique take on an old game. Instead of controlling a single snake, you must control two simultaneously. Hilarity ensues."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [seesaw "1.4.4"]] ; UI framework
   :main multi-snake.core)
--- src/multi_snake/ui.clj
+++ src/multi_snake/ui.clj
@@ -1,33 +1,29 @@
 (ns multi-snake.ui
   (:require
-    [multi-snake.game :as ms.g])
-  (:import
-    (java.awt Color Dimension GridLayout)
-    (javax.swing SwingUtilities JFrame JPanel BorderFactory)))
+    ;[seesaw.dev :as ss.dev]
+    [multi-snake.game :as ms.g]
+    [seesaw.border :as ss.b :only line-border]
+    [seesaw.core :as ss]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Configuration
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def LOGGING_ENABLED false)
-(def FPS 10)
-(def CELL_SIZE 25)
-(def DEFAULT_COLOR Color/WHITE)
-(def PLAYER_COLOR Color/BLACK)
+(def DEFAULT_COLOR :white)
+(def PLAYER_COLOR :black)
+(def ^:dynamic *FPS* 10)
+(def ^:dynamic *CELL_SIZE* 25)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Helpers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Initialize now and then use as a pseudo-singleton throughout
-(def FRAME (JFrame.))
+(def FRAME (ss/frame))
 
 ; Keep these refs around for easy retrieval
-(def N->CELL (atom {}))
-
-(defmacro run-on-ui-thread
-  [& body]
-  `(SwingUtilities/invokeLater (fn [] (do ~@body))))
+(def N->CELL (atom []))
 
 (defn- log
   [& args]
@@ -39,17 +35,15 @@
   [x y max-x]
   (+ x (* max-x y)))
 
-(defn- paint-cell
+(defn- paint-cell!
   [cell is-player-pos?]
-  (doto cell
-    (.setBackground (if is-player-pos? PLAYER_COLOR DEFAULT_COLOR))))
+  (ss/config! cell :background (if is-player-pos? PLAYER_COLOR DEFAULT_COLOR)))
 
 (defn- new-cell
   []
-  (doto (JPanel.)
-    (.setBorder (BorderFactory/createLineBorder Color/BLACK))))
+  (ss/xyz-panel :border (ss.b/line-border :color :black)))
 
-(defn- paint-panel
+(defn- paint-panel!
   "Paint all the cells for the given panel"
   [panel num-cells-wide num-cells-high player-pos]
   (log "paint-panel")
@@ -58,16 +52,16 @@
     (let [n (xy->n x y num-cells-wide)
           cell (@N->CELL n)
           is-player-pos? (= {:x x :y y} player-pos)]
-      (paint-cell cell is-player-pos?)))
+      (paint-cell! cell is-player-pos?)))
   panel)
 
-(defn- fill-panel
+(defn- fill-panel!
   "Add physical cell views to the given panel"
   [panel num-cells]
   (log "fill-panel")
   (dotimes [n num-cells]
     (let [cell (new-cell)]
-      (.add panel cell)
+      (ss/add! panel cell)
       (swap! N->CELL assoc n cell)))
   panel)
 
@@ -76,47 +70,46 @@
   (log "game->jpanel")
   (let [{:keys [width height]} (:board game)
         player-pos (:player-pos game)
-        panel (JPanel. (GridLayout. width height) true)]
-    (doto panel
-      (.setPreferredSize (Dimension. (* CELL_SIZE width)
-                                     (* CELL_SIZE height)))
-      (fill-panel (* width height))
-      (paint-panel width height player-pos))))
+        panel (ss/grid-panel :columns width :rows height)]
+    (-> panel
+        (ss/config! :size [(* *CELL_SIZE* width) :by (* *CELL_SIZE* height)])
+        (fill-panel! (* width height))
+        (paint-panel! width height player-pos))))
 
-(defn- update-frame
+(defn- update-frame!
   [game]
   (log "update-frame")
   (let [{:keys [width height]} (:board game)
         player-pos (:player-pos game)]
-    (paint-panel (.getComponent FRAME 0) width height player-pos)
-    (doto FRAME
-      (.repaint))))
+    (paint-panel! (ss/select FRAME [:#board]) width height player-pos)
+    (ss/repaint! FRAME)))
 
 (defn- game->jframe
   [game]
   (log "game->jframe")
-  (doto FRAME
-    (.add (game->jpanel game))
-    (.pack)
-    (.setTitle "Multi-Snake!")
-    (.setResizable false)
-    (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-    (.setVisible true)))
+  (-> FRAME
+      (ss/config! :title "Multi-Snake!"
+                  :content (game->jpanel game)
+                  :resizable? false
+                  :on-close :exit
+                  :id :#board)
+      ss/pack!
+      ss/show!))
 
 (defn- render-update
   "Make changes to any UI components that may have changed between frames."
   [game]
   (log "-------------")
   (log "render-update")
-  (run-on-ui-thread
-    (update-frame game)))
+  (ss/invoke-soon
+    (update-frame! game)))
 
 (defn- setup-ui
   "Setup UI with necessary components before game is actually kicked off."
   [game]
   (log "--------------")
   (log "render-initial")
-  (run-on-ui-thread
+  (ss/invoke-now ; make sure we are setup before continuing to run the game
     (game->jframe game)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -129,6 +122,6 @@
   (setup-ui initial-game)
   (loop [game initial-game]
     (render-update game)
-    (Thread/sleep (/ 1000 FPS))
+    (Thread/sleep (/ 1000 *FPS*))
     (recur (ms.g/tick game))))
 
